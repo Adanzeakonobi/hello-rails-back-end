807e7fb38b49b7b20b23f7cabf80577ca6cb5993
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-09-14 09:05:08 -0600

    Install controllers and models

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+Message.create(body: 'Ututu Oma')
+Message.create(body: 'Ibola Chi')
+Message.create(body: 'Good Evening')
+Message.create(body: 'Odaro')
+Message.create(body: 'Good evening')
